d7348e4037114d1788125cc8364005a94fed2e73
ArpitGupta1996 <68arpit@gmail.com>
2024-01-29 21:59:03 +0530

    whatsapp integration done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -136,5 +136,37 @@
                 </div>
             </div>
         </div>
+
+         {{-- whataspp integration added here --}}
+
+            <!--Jquery-->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+            <!--Floating WhatsApp css-->
+            <link rel="stylesheet" href="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.css">
+            <!--Floating WhatsApp javascript-->
+            <script type="text/javascript" src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.js"></script>
+
+        {{-- till here --}}
+
+
+        <div id="WAButton"></div>
     </body>
+
+      {{-- below script is for the whatsapp integration --}}
+      <script type="text/javascript">
+        $(document).ready(function(){
+           $('#WAButton').floatingWhatsApp({
+                phone: '+919529756684', //WhatsApp Business phone number International format-
+                 headerTitle: 'Chat with us on WhatsApp!', //Popup Title
+                popupMessage: 'Hello, how can we help you?', //Popup Message
+                showPopup: true, //Enables popup display
+                 buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
+                // position: "left"
+                position:"right",
+
+
+            });
+        });
+    </script>
+    {{-- till here is for the whatsapp integration --}}
 </html>
